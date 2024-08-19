0e21e611f87e446ac748fe21ec5f86b3a4515b19
Victor Adewale <afuwapeadewale9@gmail.com>
2024-08-19 14:13:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const toggleMenu = () => {
+    document.body.classList.toggle("open");
+};
+
+
+
+const carousel = document.querySelector('.carousel');
+const images = [
+    'assets/kemsan.JPG',
+    'residence-2219972.JPG',
+    'assets/bg3.JPG'
+];
+
+let currentIndex = 0;
+
+function updateBackground(index) {
+    carousel.style.backgroundImage = `url(${images[index]})`;
+    carousel.style.backgroundPosition = `bottom`   
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateBackground(currentIndex);
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateBackground(currentIndex);
+});
+
+// Initial background setup
+updateBackground(currentIndex);
